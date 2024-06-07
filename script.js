4ec5d940883e0de301b6fc607004a657c1e388dd
shiwanshtiwari <154734906+shiwanshtiwari@users.noreply.github.com>
2024-06-07 18:28:02 +0530

    my project

---
+++ script.js
@@ -0,0 +1,39 @@
+// console.log("hello world")
+// console.log(3+5)
+// fullname="shiwansh"
+// console.log(fullname)
+// let name="tony stark" ;
+// console.log(name);
+// const student
+// const student = {
+//     fullname:"shiwansh",
+
+//     age:24,
+//     cgpa:9,
+
+
+// };
+// comsole.log(student[age])
+// const proudct={
+//     tittle : "a future generation pen",
+//     rating : 4-4.5,
+//     offer: 60% "off",
+//     price: 256,
+// };
+// console.log( product ) arthemetic operators
+// let a=5;
+// let b=6;
+// console.log("a + b = ",a + b)
+// let a=5;
+// let b=6;
+// a++; increment and decremnet is while you do minus minus
+// console.log("a + b = ",a)
+// let a=5;
+// let b=6;
+// a**b ;
+// console.log("a + b = ",a)
+// arthemetic operator
+// let a=5;
+// let b=6;
+// a+=5;
+// console.log("a = ",a);
